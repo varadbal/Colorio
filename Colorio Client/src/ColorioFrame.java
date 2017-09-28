0f68027863626d1f7ad9674542589c7e0e89a2a5
raczboldizsar <31954796+raczboldizsar@users.noreply.github.com>
2017-09-28 11:48:35 +0200

    First steps
    300*300 Window

---
+++ Colorio Client/src/ColorioFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class ColorioFrame extends JFrame {
+    ColorioFrame(){
+        super("Colorio");
+        setSize(300,500);
+    }
+}
--- Colorio Client/src/Main.java
+++ Colorio Client/src/Main.java
@@ -1,3 +1,6 @@
 public class Main {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        ColorioFrame colorioFrame = new ColorioFrame();
+        colorioFrame.setVisible(true);
+    }
 }
